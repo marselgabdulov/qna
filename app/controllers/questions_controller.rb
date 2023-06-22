cc24b32d14333cba9dfa53251b9b694d25d8be29
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-06-22 08:46:47 +0300

    feature update question with ajax

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,9 +14,6 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
-  def edit
-  end
-
   def create
     @question = current_user.questions.new(question_params)
 
@@ -28,11 +25,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
-      redirect_to @question
-    else
-      render :edit
-    end
+    @question.update(question_params)
   end
 
   def destroy
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -19,7 +19,7 @@ feature 'User can create question', %q{
     scenario 'asks a question' do
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'some text'
-      click_on 'Ask'
+      click_on 'Save'
 
       expect(page).to have_content 'Your question successfully created.'
       expect(page).to have_content 'Test question'
@@ -27,7 +27,7 @@ feature 'User can create question', %q{
     end
 
     scenario 'asks a question whith errors' do
-      click_on 'Ask'
+      click_on 'Save'
 
       expect(page).to have_content "Title can't be blank"
     end
