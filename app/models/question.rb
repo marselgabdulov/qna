b9d3602d0d1a444ba082974bdab3415503480c47
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-06-23 17:10:51 +0300

    add nested formes screencast code

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,10 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  has_many :links, dependent: :destroy
 
   has_many_attached :files
+
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   belongs_to :user
 
---
+++ spec/features/question/add_link_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'User can add links to the question', %q{
+  In order to provide additional info to my question
+  As a question's author
+  I'd like to be able to add links
+} do
+
+  given(:user) { create(:user) }
+  given(:gist_url) { 'https://gist.github.com/marselgabdulov/6e53bf179913111403653467a6308f8a' }
+
+  scenario 'User adds link when asks the question' do
+    sign_in(user)
+    visit new_question_path
+
+    fill_in 'Title', with: 'Question Title'
+    fill_in 'Body', with: 'Question Body'
+
+    fill_in 'Link Name', with: 'My Gist'
+    fill_in 'Url', with: gist_url
+
+    click_on 'Save'
+
+    expect(page).to have_link 'My Gist', href: gist_url
+  end
+
+end
