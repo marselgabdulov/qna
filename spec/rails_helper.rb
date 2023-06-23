ebbf6f32e67362cbb39a3fd55867765f95ea692c
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-06-23 05:00:41 +0300

    add attach multiple files screencast code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,11 +68,14 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
+  config.after(:all) do
+    FileUtils.rm_rf("#{Rails.root}/tmp/storage")
   end
+end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
