dc777f70be28b7c7579493f008253caa4468c611
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-06-23 07:04:05 +0300

    add 'author could attach files on editing his question' scenario

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -31,6 +31,14 @@ feature 'User can edit his question', %q{
       expect(page).to have_content("Title can't be blank")
     end
 
+    scenario 'tries to edit the question with attached file', js: true do
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Save'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
+
   end
 
   scenario 'Not an author tries to edit the question' do
