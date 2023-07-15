f5b9fdced83eafba994724431c5f647519ba5b04
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-07-15 06:51:11 +0300

    add devise confirmation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,4 +8,10 @@ FactoryBot.define do
     password { '12345678' }
     password_confirmation { '12345678' }
   end
+
+  after(:create) { |user| user.confirm }
+
+  trait :unconfirmed do
+    confirmed { false }
+  end
 end
