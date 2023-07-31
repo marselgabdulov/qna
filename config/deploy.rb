afc6351c109fc524c431beeee4fb921d69baa1cc
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-07-31 08:52:19 +0300

    change server user name

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,8 @@ set :repo_url, "git@github.com:marselgabdulov/qna.git"
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/qna"
-set :deploy_user, "deploy"
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, "deployer"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
