44a30b4a88ab54f4b39ad8f4ab0e23900cda70f1
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2023-08-02 10:25:39 +0300

    add caching screencast code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,11 @@ module ApplicationHelper
   def time_ago(resourse_created_at)
     "#{distance_of_time_in_words(Time.now, resourse_created_at)} ago"
   end
+
+  def collection_cache_key_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,7 @@ class Answer < ApplicationRecord
   include Votable
   include Commentable
 
-  belongs_to :question
+  belongs_to :question, touch: true
 
   validates :body, presence: true
 
